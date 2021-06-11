15445b848d106ab5b1fba30c84544cfe59d568b4
cakekindel <cakekindel@users.noreply.github.com>
2021-06-11 15:31:41 +0000

    chore(release): 1.0.1

---
+++ target/debug/deps/happi-73cad837955c6188.d
@@ -0,0 +1,5 @@
+/home/runner/work/hapi-rs/hapi-rs/target/debug/deps/happi-73cad837955c6188.rmeta: src/lib.rs
+
+/home/runner/work/hapi-rs/hapi-rs/target/debug/deps/happi-73cad837955c6188.d: src/lib.rs
+
+src/lib.rs:
