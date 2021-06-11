7bded3e52a5081b0beab8e9a3c365272b31d393a
Orion Kindel <cakekindel@gmail.com>
2021-06-11 08:21:16 -0700

    initial commit

---
+++ target/debug/deps/hapi-efadf325ea65ec9a.d
@@ -0,0 +1,5 @@
+/home/orion/src/personal/hapi-rs/target/debug/deps/hapi-efadf325ea65ec9a.rmeta: src/lib.rs
+
+/home/orion/src/personal/hapi-rs/target/debug/deps/hapi-efadf325ea65ec9a.d: src/lib.rs
+
+src/lib.rs:
