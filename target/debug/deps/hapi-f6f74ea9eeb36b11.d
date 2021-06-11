49904af47ac36cf92812edbce034055258624eda
Orion Kindel <cakekindel@gmail.com>
2021-06-11 08:38:14 -0700

    please delete this

--- target/debug/deps/hapi-f6f74ea9eeb36b11.d
+++
@@ -1,5 +0,0 @@
-/home/orion/src/personal/hapi-rs/target/debug/deps/hapi-f6f74ea9eeb36b11.rmeta: src/lib.rs
-
-/home/orion/src/personal/hapi-rs/target/debug/deps/hapi-f6f74ea9eeb36b11.d: src/lib.rs
-
-src/lib.rs:
