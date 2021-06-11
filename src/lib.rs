0666a09a9d792af5dbdee56645a126fc60b4327a
Orion Kindel <cakekindel@gmail.com>
2021-06-11 08:26:11 -0700

    add lints

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,16 @@
 //! |Github Repo Secret|CARGO_TOKEN|Token issued for your user by crates.io|[How-To](https://doc.rust-lang.org/cargo/reference/publishing.html#before-your-first-publish)|
 //! |Github Repo Secret|GH_TOKEN|A GitHub PAT|[How-To](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token)|
 
-// todo!
+#![doc(html_root_url = "https://docs.rs/slack-blocks/0.25.0")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+// #![feature(doc_cfg)] // for local docs
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(unreachable_pub)]
+#![warn(unused_crate_dependencies)]
+#![deny(missing_docs)]
+#![forbid(unsafe_code)]
+
+/// TODO
+#[derive(Clone, Copy, Debug)]
+pub struct Todo;
