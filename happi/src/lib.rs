c222866eeb266c4e43a179f71d1afab5b11917d7
Orion Kindel <cakekindel@gmail.com>
2021-06-12 13:22:51 -0700

    docs: add readme example

--- happi/src/lib.rs
+++ happi/src/lib.rs
@@ -2,11 +2,55 @@
 //!
 //! This includes a CI/CD pipeline, README templating, and cargo-make scripts.
 //!
-//! # Setup
-//! |Type|Name|Value|How-To|
-//! |--|--|--|--|
-//! |Github Repo Secret|CARGO_TOKEN|Token issued for your user by crates.io|[How-To](https://doc.rust-lang.org/cargo/reference/publishing.html#before-your-first-publish)|
-//! |Github Repo Secret|GH_TOKEN|A GitHub PAT|[How-To](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token)|
+//! # Example
+//! Using an example of <https://reqres.in/>, a `happi` implementation of this would look **something** like:
+//!
+//! ```rust ignore
+//! pub fn main() -> Result<(), dyn std::error::Error> {
+//!   let reqres = ReqResApi(reqwest::blocking::Client::new());
+//!
+//!   let user_page = reqres.get_all_users(None)?;
+//!
+//!   println!("{:#?}", user_page);
+//! }
+//!
+//! // This is the *real* client that hits the API
+//! #[happi(base_url = "https://reqres.in/api")]
+//! pub struct ReqResApi(#[client] reqwest::blocking::Client);
+//!
+//! // This is a trait for the `user` resource that `happi`
+//! // will implement for `ReqResApi`.
+//! //
+//! // When you want to use this resource, your function can
+//! // accept an `impl reqres::UserResource`, accepting the real
+//! // deal or a mock when you write tests.
+//! #[happi(api = ReqResApi, resource = "/users")]
+//! pub trait UserResource {
+//!   #[get]
+//!   pub fn get_all_users(&self, #[query] page: Option<u32>) -> Result<UserPage, happi::Error>
+//!
+//!   #[get("/{id}")]
+//!   #[when(status == 404, invoke = |_resp| Ok(None))]
+//!   pub fn get_user(&self, id: u32) -> Result<Option<User>, happi::Error>
+//! }
+//!
+//! #[derive(Debug, Serialize, Deserialize)]
+//! pub struct User {
+//!   id: u32,
+//!   first_name: String,
+//!   last_name: String,
+//!   avatar: String,
+//! }
+//!
+//! #[derive(Debug, Serialize, Deserialize)]
+//! pub struct UserPage {
+//!   page: u32,
+//!   per_page: u32,
+//!   total: u32,
+//!   total_pages: u32,
+//!   data: Vec<User>
+//! }
+//! ```
 
 #![doc(html_root_url = "https://docs.rs/happi/0.0.4")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
